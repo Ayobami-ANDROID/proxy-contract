58b92d4562d46e1e34ad4898e0ac5fb324c935bc
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-06-08 22:42:04 +0100

    proxy

--- contracts/first.sol
+++ contracts/first.sol
@@ -1,52 +1,31 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract First is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
-    // Change to private visibility
-    uint256 private _value1;
+    uint256 private _value;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    // Initializes the contract
     function initialize() public initializer {
-        __Context_init_unchained();
         __ERC20_init_unchained("MyToken", "MTK");
         __Ownable_init_unchained();
     }
 
-    // Stores a new value in the contract
-    function store(uint256 newValue) public {
-        _value1 = newValue; // Update the state variable
+    function store(uint256 newValue) public onlyOwner {
+        _value = newValue;
         emit ValueChanged(newValue);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
-        return _value1; // Retrieve the updated state variable
+        return _value;
     }
 
-    // Overrides the _beforeTokenTransfer function from ERC20Upgradeable
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Upgradeable) {
-        super._beforeTokenTransfer(from, to, amount);
-
-        // Additional custom logic for token transfer
-    }
-
-    // Overrides the upgradeTo function from UUPSUpgradeable
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        override
-        onlyOwner
-    {}
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     // Additional custom functions or overrides can be added as needed
 }
